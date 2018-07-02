e42f883564ff42deccd51d1566645872687a4084
Vincent Guo <vincent.weiguo@gmail.com>
2018-07-02 16:33:21 +0800

    Create the InventoryApp component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InventoryAppComponent } from './inventory-app/inventory-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InventoryAppComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/inventory-app/inventory-app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory-app',
+  templateUrl: './inventory-app.component.html',
+  styleUrls: ['./inventory-app.component.css']
+})
+export class InventoryAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
