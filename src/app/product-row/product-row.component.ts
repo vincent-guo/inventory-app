0c612de86d779e6d3214b3cf5c2f4600a0572a71
Vincent Guo <vincent.weiguo@gmail.com>
2018-07-03 10:49:51 +0800

    Modify the ProductRow component

--- src/app/product-row/product-row.component.ts
+++ src/app/product-row/product-row.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Product} from '../product';
 
 @Component({
   selector: 'app-product-row',
+  host: {'class': 'item'},
   templateUrl: './product-row.component.html',
   styleUrls: ['./product-row.component.css']
 })
 export class ProductRowComponent implements OnInit {
+  @Input() product: Product;
 
   constructor() { }
 
