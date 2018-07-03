036fd4a4ebcce81f5d201cc2f0b7f4e47c42be12
Vincent Guo <vincent.weiguo@gmail.com>
2018-07-03 10:22:17 +0800

    Create the ProductRow component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { InventoryAppComponent } from './inventory-app/inventory-app.component';
 import { ProductsListComponent } from './products-list/products-list.component';
+import { ProductRowComponent } from './product-row/product-row.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InventoryAppComponent,
-    ProductsListComponent
+    ProductsListComponent,
+    ProductRowComponent
   ],
   imports: [
     BrowserModule
