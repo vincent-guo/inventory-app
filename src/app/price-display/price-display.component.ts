b9467266afc070d7a06d9d9be1fc72cc68c635bb
Vincent Guo <vincent.weiguo@gmail.com>
2018-07-03 11:05:51 +0800

    Modify the PriceDisplay component

--- src/app/price-display/price-display.component.ts
+++ src/app/price-display/price-display.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-price-display',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./price-display.component.css']
 })
 export class PriceDisplayComponent implements OnInit {
+  @Input() price: number;
 
   constructor() { }
 
