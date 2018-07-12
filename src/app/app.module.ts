5fc1ba1c52e8cd43acb96947aae3b29ce7e6a68a
Vincent Guo <vincent.weiguo@gmail.com>
2018-07-12 10:41:51 +0800

    Moved app-inventory-root from src/app/inventory-app to src/app/app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { InventoryAppComponent } from './inventory-app/inventory-app.component';
 import { ProductsListComponent } from './products-list/products-list.component';
 import { ProductRowComponent } from './product-row/product-row.component';
 import { ProductImageComponent } from './product-image/product-image.component';
@@ -12,7 +11,6 @@ import { ProductDepartmentComponent } from './product-department/product-departm
 @NgModule({
   declarations: [
     AppComponent,
-    InventoryAppComponent,
     ProductsListComponent,
     ProductRowComponent,
     ProductImageComponent,
